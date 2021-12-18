1d112e242cc524416610335eea22101efdf12e6a
lee_junha <lee_junha@ritoshikazunoMacBook-Pro.local>
2021-12-18 10:50:32 +0900

    初期setup

---
+++ HelloJavaTest/src/App.java
@@ -0,0 +1,13 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("文字列入力");
+        String strValue = in.readLine();
+        System.out.println("文字列出力：" + strValue );
+    }
+}
