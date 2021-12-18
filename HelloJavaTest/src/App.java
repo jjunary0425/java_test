da82418f49b46eb47b45965492b11007b15224ee
lee_junha <lee_junha@ritoshikazunoMacBook-Pro.local>
2021-12-18 10:58:07 +0900

    コメントの追加

--- HelloJavaTest/src/App.java
+++ HelloJavaTest/src/App.java
@@ -3,9 +3,14 @@ import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // BufferedReaderを生成する。
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        // 文字列の入力をもらう。
         System.out.println("文字列入力");
         String strValue = in.readLine();
+
+        // 文字列を出力する。
         System.out.println("文字列出力：" + strValue );
     }
 }
